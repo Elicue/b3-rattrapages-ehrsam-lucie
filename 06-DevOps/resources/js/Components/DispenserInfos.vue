391285a5308de80938157dd9592b9f4b80bb34ea
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2024-07-30 17:34:35 +0200

    :lipstick: Quick Home Front

--- 06-DevOps/resources/js/Components/DispenserInfos.vue
+++ 06-DevOps/resources/js/Components/DispenserInfos.vue
@@ -1,8 +1,10 @@
 <template>
-  <div style="display: flex;flex-direction: row; align-items: center; gap: 1vw;">
-      <p>{{ dispenser.name }}</p>
-      <p>{{ dispenser.location }}</p>
-      <button style="height: fit-content;">{{ dispenser.status }}</button>
+  <div class="shadow-[0px_0px_20px_0px_rgba(229,231,235,0.3)] rounded-lg transition-all duration-500 bg-white p-6 flex flex-col justify-between w-full h-[17vh] hover:shadow-[0px_0px_20px_0px_rgba(229,231,235,0.5)]" :class="{'border-gray-200  border duration-100' : active }">
+    <div class="flex flex-row justify-between items-center">
+      <p class="font-semibold">{{ dispenser.name }}</p>
+      <button class="text-[#0311a5] border-[1.5px] rounded-md px-5 py-1 uppercase font-semibold border-[#0311a5]">{{ dispenser.status }}</button>
+    </div>
+      <p class="flex flex-row gap-4 items-center"><img class="w-4 h-fit" src="https://www.iconpacks.net/icons/2/free-location-icon-2952-thumb.png" alt="location">{{ dispenser.location }}</p>
   </div>
 </template>   
 
