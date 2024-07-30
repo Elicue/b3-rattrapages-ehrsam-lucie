d2ecef1799e883387c9b620e45f22423f194d368
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2024-07-30 17:38:45 +0200

    :iphone: Quick Responsive

--- 06-DevOps/resources/js/Components/DispenserInfos.vue
+++ 06-DevOps/resources/js/Components/DispenserInfos.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shadow-[0px_0px_20px_0px_rgba(229,231,235,0.3)] rounded-lg transition-all duration-500 bg-white p-6 flex flex-col justify-between w-full h-[17vh] hover:shadow-[0px_0px_20px_0px_rgba(229,231,235,0.5)]" :class="{'border-gray-200  border duration-100' : active }">
+  <div class="shadow-[0px_0px_20px_0px_rgba(229,231,235,0.3)] rounded-lg transition-all duration-500 bg-white p-6 flex flex-col justify-between w-full lg:h-[17vh] h-[20vh] hover:shadow-[0px_0px_20px_0px_rgba(229,231,235,0.5)]" :class="{'border-gray-200  border duration-100' : active }">
     <div class="flex flex-row justify-between items-center">
       <p class="font-semibold">{{ dispenser.name }}</p>
       <button class="text-[#0311a5] border-[1.5px] rounded-md px-5 py-1 uppercase font-semibold border-[#0311a5]">{{ dispenser.status }}</button>
--- 06-DevOps/resources/js/Layouts/MainLayout.vue
+++ 06-DevOps/resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen bg-[#f9f9f7]">
-    <nav class="flex flex-row justify-between px-12 py-4 border-b-[1.5px] border-gray-200 bg-white">
+  <div class="min-h-screen bg-[#f9f9f7] pb-8">
+    <nav class="flex flex-row justify-between lg:px-12 px-8 py-4 border-b-[1.5px] border-gray-200 bg-white">
       <div class="menu flex flex-row gap-2 items-center">
         <img class="w-10 mr-8 h-fit" src="https://upload.wikimedia.org/wikipedia/fr/thumb/e/e1/Logo_Picard_surgel%C3%A9s.png/1260px-Logo_Picard_surgel%C3%A9s.png?20170717143342" alt="logo picard">
         <Link class="text-[#0311a3] font-semibold" href="/">Home Page</Link>&nbsp;
