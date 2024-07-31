a360218dfbb8e23b7a2ea5a6eba8892a694e1ad2
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2024-07-31 18:28:26 +0200

    :lipstick: Quick Button Changes

--- 06-DevOps/resources/js/Components/DispenserInfos.vue
+++ 06-DevOps/resources/js/Components/DispenserInfos.vue
@@ -2,14 +2,29 @@
   <div class="shadow-[0px_0px_20px_0px_rgba(229,231,235,0.3)] rounded-lg transition-all duration-500 bg-white p-6 flex flex-col justify-between w-full lg:h-[17vh] h-[20vh] hover:shadow-[0px_0px_20px_0px_rgba(229,231,235,0.5)]" :class="{'border-gray-200  border duration-100' : active }">
     <div class="flex flex-row justify-between items-center">
       <p class="font-semibold">{{ dispenser.name }}</p>
-      <button class="text-[#0311a5] border-[1.5px] rounded-md px-5 py-1 uppercase font-semibold border-[#0311a5]">{{ dispenser.status }}</button>
+      <button :class="statusClass" class="text-[#0311a5] border-[1.5px] rounded-md px-5 py-1 uppercase font-semibold border-[#0311a5]">{{ dispenser.status }}</button>
     </div>
       <p class="flex flex-row gap-4 items-center"><img class="w-4 h-fit" src="https://www.iconpacks.net/icons/2/free-location-icon-2952-thumb.png" alt="location">{{ dispenser.location }}</p>
   </div>
 </template>   
 
 <script setup>
-  defineProps({
+  import { defineProps, computed } from 'vue';
+  
+  // Define props
+  const props = defineProps({
     dispenser: Object,
   });
-</script>
+
+  // Use props.dispenser to access the dispenser object
+  const statusClass = computed(() => {
+    switch (props.dispenser.status.toLowerCase()) {
+      case 'active':
+        return ' text-green-500 border-green-500';
+      case 'inactive':
+        return ' text-red-500 border-red-500';
+      case 'pending':
+        return ' text-yellow-500 border-yellow-500';
+    }
+  });
+</script
